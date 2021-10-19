665925da9de8a31e159b57a3eeede2515cba5171
CatCh072 <91379092+CatCh072@users.noreply.github.com>
2021-10-18 20:25:02 -0400

    created JS for menu
    created mobile menu toggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,12 @@
 console.log("Welcome to CC's Portfolio Site")
 //Exercise 2.1-Intro to Java Script-Task
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+    } else {
+        x.className = 'navtoggle';
+    }    
+}
+//Exercise 2.3-JavaScript Libraries & Plugins-Task
