7bd21156ef0e61a0c635c35a6c4a92e36bdf9f56
CatCh072 <91379092+CatCh072@users.noreply.github.com>
2021-10-19 20:13:24 -0400

    create fix to nav-contact
    fixed "contact" jQuery for 640+ screens

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 console.log("Welcome to CC's Portfolio Site")
 //Exercise 2.1-Intro to Java Script-Task
 
+$('#heading-text').text('How to create a hamburger menu with jQuery');
+
 function menuToggle() {
     var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
+        var isMobileBreakpoint = window.innerWidth <= 641
+
+    if (x.className === 'navtoggle' && isMobileBreakpoint) {
         x.className += ' responsive';
     } else {
         x.className = 'navtoggle';
