a2508e12ffc87dc4ec0bea28448423dce9c40ed7
CatCh072 <91379092+CatCh072@users.noreply.github.com>
2021-11-16 22:03:13 -0500

    create updates and form
    created updates on Olive, links.
    created contact form

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,24 @@ function menuToggle() {
     }
 }
 //Exercise 2.3-JavaScript Libraries & Plugins-Task
+
+var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(success => {
+        status.innerHTML = "Thanks for your message!";
+        form.reset()
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem."
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
